d56c823c92bc99fe48579e93f06bacc0b811ce11
Xiao Li <xli87@bu.edu>
2018-01-08 12:27:34 -0500

    update

--- test/run_mac.sh
+++ test/run_mac.sh
@@ -7,10 +7,17 @@
 #--env="DISPLAY=${ip}:0"\
 #export containerID=$(docker ps -l -q)
 
+# DIR is the directory of the current file
+DIR="$( cd "$( dirname "${BASH_SOURCE[ 0]}" )" && pwd )"
+
+DOCKER_ROS_DIR="$(dirname "$DIR")"
+
+DOCKER_DIR="$(dirname "$DOCKER_ROS_DIR")"
+
 docker run -it \
        --rm \
-       -v="/Users/xiaoli/src/docker_home:/home/xli4217/" \
+       -v="$DOCKER_DIR/docker_home/:/home/xli4217/" \
        -w="/home/xli4217" \
-       --name ros_test ros_test \
+       --name ros_test1 ros_test \
    
 
