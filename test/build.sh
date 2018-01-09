ef39eecf28e6e98b552b499eb41b5c81055671e8
burobotics <burobotics>
2018-01-08 19:32:49 -0500

    graphic to host works in test/test/*

--- test/build.sh
+++ test/build.sh
@@ -1,4 +1,6 @@
-docker build --rm -t ros_test .
+#!/bin/bash
+
+docker build --rm -t  ros_test --build-arg USER=burobotics --build-arg UID=$UID --file="Dockerfile.gpu" .
 
 echo -e "-- Removing exited containers --\n"
 docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm
@@ -6,8 +8,8 @@ docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm
 echo -e "\n\n-- Removing untagged images --\n"
 docker rmi --force $(docker images | awk '/^<none>/ { print $3 }')
 
-echo -e "\n\n-- Removing volume directories --\n"
-docker volume rm $(docker volume ls --quiet --filter="dangling=true")
+# echo -e "\n\n-- Removing volume directories --\n"
+# docker volume rm $(docker volume ls --quiet --filter="dangling=true")
 
 echo -e "\n\nDone"
 
--- test/run_linux.sh
+++ test/run_linux.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 
-docker run -it \
-       --rm \
-       -v="/Users/xiaoli/src/docker_ros/docker_home:/home/xli4217/" \
-       -v="/tmp/.X11-unix:/tmp.X11-unix:rw"\
-       --env="DISPLAY"\
-       --env="QT_X11_NO_MITSHM=1"\
-       --name ros_test ros_test \
-       rqt
-export containerID=$(docker ps -l -q)
+DIR="$( cd "$( dirname "${BASH_SOURCE[ 0]}" )" && pwd )"
+
+DOCKER_ROS_DIR="$(dirname "$DIR")"
+
+DOCKER_DIR="$(dirname "$DOCKER_ROS_DIR")"
+
+
+xhost +local:root
+
+
+nvidia-docker run -it --rm \
+--env="DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--workdir="/home/$USER" \
+--volume="/home/$USER:/home/$USER" \
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+-e LOCAL_USER_ID=`id -u $USER` \
+-e LOCAL_GROUP_ID=`id -g $USER` \
+-e LOCAL_GROUP_NAME=`id -gn $USER` \
+--name ros_test ros_test
+
+#--volume="$DOCKER_DIR/docker_home/:/home/$USER/" \
+
+# docker run -it \
+#        --rm \
+#        --privileged \
+#        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+#        --volume="$DOCKER_DIR/docker_home/:/home/$USER/" \
+#        --env="DISPLAY"\
+#        --env="QT_X11_NO_MITSHM=1"\
+#        --name ros_test ros_test
+
+#--env="QT_X11_NO_MITSHM=1"\
+
+# docker run -it \
+#        --rm \
+#        --volume="/etc/group:/etc/group:ro"\
+#        --volume="/etc/passwd:/etc/passwd:ro" \
+#        --volume="/etc/shadow:/etc/shadow:ro" \
+#        --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+#        --volume="/tmp/.X11-unix:/tmp.X11-unix:rw"\
+#        --env="DISPLAY:$DISPLAY"\
+#        --env="QT_X11_NO_MITSHM=1"\   
+#        --volume="$DOCKER_DIR/docker_home/:/home/$USER/" \
+#        --name ros_test ros_test
+
+
+# export containerID=$(docker ps -l -q)
+# docker start $containerID
+
+xhost -local:root
