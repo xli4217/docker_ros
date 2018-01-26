375a198da5ccc67dd8330ada6314a7d8e0d53802
burobotics <burobotics>
2018-01-26 18:58:17 -0500

    update

--- run_linux.sh
+++ run_linux.sh
@@ -53,33 +53,4 @@ else
 fi
 
 
-#--volume="/home/$USER:/home/$USER" \
-
-# docker run -it \
-#        --rm \
-#        --privileged \
-#        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"\
-#        --volume="$DOCKER_DIR/docker_home/:/home/$USER/" \
-#        --env="DISPLAY"\
-#        --env="QT_X11_NO_MITSHM=1"\
-#        --name ros_test ros_test
-
-#--env="QT_X11_NO_MITSHM=1"\
-
-# docker run -it \
-#        --rm \
-#        --volume="/etc/group:/etc/group:ro"\
-#        --volume="/etc/passwd:/etc/passwd:ro" \
-#        --volume="/etc/shadow:/etc/shadow:ro" \
-#        --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-#        --volume="/tmp/.X11-unix:/tmp.X11-unix:rw"\
-#        --env="DISPLAY:$DISPLAY"\
-#        --env="QT_X11_NO_MITSHM=1"\   
-#        --volume="$DOCKER_DIR/docker_home/:/home/$USER/" \
-#        --name ros_test ros_test
-
-
-# export containerID=$(docker ps -l -q)
-# docker start $containerID
-
 xhost -local:root
