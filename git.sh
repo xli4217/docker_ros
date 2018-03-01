b9b4b119f43421a0fc98c98fffa66e8651d145b8
burobotics <burobotics>
2018-03-01 18:07:17 -0500

    update

--- git.sh
+++ git.sh
@@ -6,6 +6,8 @@
 CMD=$1 # this can be push or pull
 WS=$2 # this is the workspace name, can be 'baxter_ws', 'baxter_ws_hardware', 'parallel_baxter_ws'
 
+cd $HOME/Xiao/docker/docker_home
+
 if [ $CMD == 'push' ] #&& [ $WS == 'baxter_ws' ]
 then
     ###############
@@ -17,17 +19,17 @@ then
     git commit -m "update"
     git push origin rlfps3
 
-    if [ $WS == 'parallel_baxter_ws' ] # entire workspace needs to be pushed for this one
-    then        
-        ###########################
-        # Push parallel baxter ws #
-        ###########################
-        echo "#### Pushing parallel_baxter_ws and baxter_bridge ####"
-        cd ../parallel_baxter_ws
-        git add -A
-        git commit -m "update"
-        git push origin master
-    fi
+    # if [ $WS == 'parallel_baxter_ws' ] # entire workspace needs to be pushed for this one
+    # then        
+    #     ###########################
+    #     # Push parallel baxter ws #
+    #     ###########################
+    #     echo "#### Pushing parallel_baxter_ws and baxter_bridge ####"
+    #     cd ../parallel_baxter_ws
+    #     git add -A
+    #     git commit -m "update"
+    #     git push origin master
+    # fi
         
     ###################
     # Push docker_ros #
@@ -95,7 +97,7 @@ then
     # push baxter_learn
     echo "#### Pulling baxter_learn ####"
     cd $WS_HOME/src/baxter_learn_pkgs/baxter_learn
-    git push pull master
+    git pull origin master
 
     
 else
