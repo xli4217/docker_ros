f29d476d25000b18f9fd7f32c081875f9ce5dced
burobotics <burobotics>
2018-03-02 17:03:13 -0500

    update

--- get_data.sh
+++ get_data.sh
@@ -6,8 +6,8 @@ pc2="gyang@192.168.1.177"
 pc3="burobotics@192.168.1.233"
 guangpc="burobotics@128.197.136.202"
 
-scp -r $pc2:$HOME/Xiao/docker/docker_home/experiments/* $HOME/Xiao/docker/docker_home/experiments/
+sudo scp -r $pc2:/home/gyang/Xiao/docker/docker_home/experiments/* $HOME/Xiao/docker/docker_home/experiments/
 
-scp -r $pc3:$HOME/Xiao/docker/docker_home/experiments/* $HOME/Xiao/docker/docker_home/experiments/
+sudo scp -r $pc3:$HOME/Xiao/docker/docker_home/experiments/* $HOME/Xiao/docker/docker_home/experiments/
 
-scp -r $guangpc:$HOME/Xiao/docker/docker_home/experiments/* $HOME/Xiao/docker/docker_home/experiments/
+sudo scp -r $guangpc:$HOME/Xiao/docker/docker_home/experiments/* $HOME/Xiao/docker/docker_home/experiments/
