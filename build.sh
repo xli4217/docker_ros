0282c2fa674a58c0b97dc9092462267472e323d9
Xiao Li <xli87@bu.edu>
2018-03-20 18:00:27 -0400

    update

--- build.sh
+++ build.sh
@@ -7,21 +7,21 @@ DIRS_TO_COPY_ROOT=$3 # this is required for "deploy", not necessary for "base"
 
 if [ "$1" == "--help" ]
 then
-    echo "Usage: ./build.sh <deploy/base/rlfps> <gpu/cpu> <DIRS_TO_COPY_ROOT (this is required for deploy, not base)>"
+    echo "Usage: ./build.sh <deploy/base/rlfps> <gpu/cpu> <DIRS_TO_COPY_ROOT (this is required for deploy and rlfps, not base)>"
     exit 0
 fi
 
 if [ $IMAGE == 'base' ]
 then
-    docker build --rm -t  baxter-simulation-$ARCH --build-arg USER=$USER --build-arg UID=$UID --file="baxter-simulation/base/$ARCH/Dockerfile" .
+    docker build --rm -t  xli4217/baxter-simulation-$ARCH --build-arg USER=$USER --build-arg UID=$UID --file="baxter-simulation/base/$ARCH/Dockerfile" .
 
 elif [ $IMAGE == 'deploy' ]
 then
-    docker build --rm -t  deploy-baxter-simulation-$ARCH --build-arg DIRS_TO_COPY_ROOT=$DIRS_TO_COPY_ROOT --file="baxter-simulation/deploy/$ARCH/Dockerfile" .
+    docker build --rm -t  xli4217/deploy-baxter-simulation-$ARCH --build-arg DIRS_TO_COPY_ROOT=$DIRS_TO_COPY_ROOT --file="baxter-simulation/deploy/$ARCH/Dockerfile" .
 
 elif [ $IMAGE == 'rlfps' ]
 then
-    docker build --rm -t  rlfps-$ARCH --build-arg DIRS_TO_COPY_ROOT=$DIRS_TO_COPY_ROOT --file="rlfps-docker/$ARCH/Dockerfile" .
+    docker build --rm -t  xli4217/rlfps-$ARCH  --file="rlfps-docker/$ARCH/Dockerfile" .
 
 else
     echo "image type not found"
---
+++ cloud_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y git tmux emacs24 htop
+
+
+# install docker
+
+
